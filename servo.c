9488b71fce66384b8c90d091ac10707a373b44ae
Daniel Steuer <hk@sockenbuegeln.de>
2018-06-10 02:24:02 +0200

    support for servo poweroff (WIP)

--- servo.c
+++ servo.c
@@ -16,36 +16,16 @@ servo_t servo_p, servo_y;
 
 
 void servo_init( void ) {
-	DDRD  |= (1<<PD2); // OCR1A
-	DDRC  |= (1<<PC1); // OCR1B
-
-	servo_activate( );
-
 	servo_p.lowerLimit = 0x0500;
 	servo_p.upperLimit = 0x0F00;
 
 	servo_y.lowerLimit = 0x0370;
 	servo_y.upperLimit = 0x10F0;
 
+	servo_enable();
 
-	// setup timer 1 for servo PWM output
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-		TCCR1C = 0; // must be 0
-
-		TIMSK1 = (1<<FOC1A)|(1<<FOC1B);
-
-		// pwm output for servos (16 bit fast PWM)
-		TCCR1A  = (1<<COM1A1); // pin goes high on reset and low on compare match
-		TCCR1A |= (1<<COM1B0)|(1<<COM1B1); // pin goes low on reset and high on compare match
-		TCCR1A |= (1<<WGM11); // 16 bit fast PWM
-		TCCR1B  = (1<<WGM12)|(1<<WGM13); // 16 bit fast PWM (continued) (ICR1=TOP)
-		TCCR1B |= (1<<CS11); // clockdivider F_CPU / 8
-
-		ICR1 = 40000; // 20 ms duty cycle
-
-		OCR1A = servo_p.lowerLimit + (servo_p.upperLimit - servo_p.lowerLimit) / 2; // 1.5 ms is default
-		OCR1B = servo_y.lowerLimit + (servo_y.upperLimit - servo_y.lowerLimit) / 2; // 1.5 ms is default
-	}
+	OCR1A = servo_p.lowerLimit + (servo_p.upperLimit - servo_p.lowerLimit) / 2; // 1.5 ms is default
+	OCR1B = servo_y.lowerLimit + (servo_y.upperLimit - servo_y.lowerLimit) / 2; // 1.5 ms is default
 }
 
 
@@ -153,14 +133,35 @@ uint16_t servo_getRawValue( uint8_t servoId ) {
 
 
 
-void servo_activate( void ) {
-	DDRB  |= (1<<PB6); // servo power FET
-	PORTB |= (1<<PB6); // activate servos
+void servo_enable( void ) {
+	DDRD  |= (1<<PD2); // OCR1A
+	DDRC  |= (1<<PC1); // OCR1B
+
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    TCCR1C = 0; // must be 0
+
+    TIMSK1 = (1<<FOC1A)|(1<<FOC1B);
+
+    // pwm output for servos (16 bit fast PWM)
+    TCCR1A  = (1<<COM1A1); // pin goes high on reset and low on compare match
+    TCCR1A |= (1<<COM1B0)|(1<<COM1B1); // pin goes low on reset and high on compare match
+    TCCR1A |= (1<<WGM11); // 16 bit fast PWM
+    TCCR1B  = (1<<WGM12)|(1<<WGM13); // 16 bit fast PWM (continued) (ICR1=TOP)
+    TCCR1B |= (1<<CS11); // clockdivider F_CPU / 8
+
+    ICR1 = 40000; // 20 ms duty cycle
+  }
 }
 
 
 
-void servo_deActivate( void ) {
-	DDRB  |=  (1<<PB6); // servo power FET
-	PORTB &= ~(1<<PB6); // activate servos
+void servo_disable( void ) {
+	TCCR1B &= ~( (1<<CS12) | (1<<CS11) | (1<<CS10) ); // deactivate PWM clock
+
+	// float both PWM pins
+	DDRD   &= ~(1<<PD2);
+	PORTD  &= ~(1<<PD2);
+
+	DDRC   &= ~(1<<PC1);
+	PORTC  &= ~(1<<PC1);
 }
--- servo.h
+++ servo.h
@@ -7,8 +7,8 @@ void servo_calibrateUpperLimit( void );
 void servo_setValue( uint8_t servoId, uint8_t value );
 void servo_setRawValue( uint8_t servoId, uint16_t value );
 uint16_t servo_getRawValue( uint8_t servoId );
-void servo_activate( void );
-void servo_deActivate( void );
+void servo_enable( void );
+void servo_disable( void );
 
 #else
 #error "double include of setup.h"
