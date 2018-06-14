5cf2469eb911968deb6817c84c701ee03c9a61fd
Daniel Steuer <hk@sockenbuegeln.de>
2018-06-14 16:09:07 +0200

    add interface to erase eeprom

--- eeprom.c
+++ eeprom.c
@@ -30,11 +30,13 @@ void eeprom_readConfig( void ) {
 	else {
 		// no config in eeprom. Use defaults
 		config.data.hwId = 0;
-		config.data.pMin = 0x0500;
-		config.data.pMax = 0x0F00;
-		config.data.yMin = 0x0370;
-		config.data.yMax = 0x10F0;
+		config.data.inUseMarker = 0;
+		config.data.pMin = 0x05AA;
+		config.data.pMax = 0x0FAA;
+		config.data.yMin = 0x04AA;
+		config.data.yMax = 0x10AA;
 	}
+
 }
 
 
@@ -44,5 +46,14 @@ void eeprom_writeConfig( void ) {
 
 	for(uint8_t i=0; i < sizeof(config); ++i ) {
 		eeprom_write_byte((uint8_t *)(EEPROM_CONFIG_ADDR + i), config.raw[i] );
+	}
+}
+
+
+
+void eeprom_erase( void ) {
+	for(uint8_t i=0; i < sizeof(config); ++i ) {
+		eeprom_update_byte(  (uint8_t *)(EEPROM_CONFIG_ADDR + i), 0xFF );
+	//	eeprom_update_byte( (uint8_t *)(EEPROM_CONFIG_ADDR + i), 0xFF );
 	}
 }
--- eeprom.h
+++ eeprom.h
@@ -5,7 +5,7 @@
 
 void eeprom_readConfig( void );
 void eeprom_writeConfig( void );
-
+void eeprom_erase( void );
 
 
 #endif // __EEPROM_H__
